966c19c5f52a0b05afcfad03e3cd446714191911
Valery Shcherbinin <fundroin@mail.ru>
2022-06-27 20:06:58 +0500

    настраивает проект и добавляет иконки и картинки

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from '@/router'
+import componentsUI from '@/components/UI'
+import store from '@/store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+
+componentsUI.forEach(component => {
+    app.component(component.name, component);
+});
+
+app
+    .use(store)
+    .use(router)
+    .mount('#app');
